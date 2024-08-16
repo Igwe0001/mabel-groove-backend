853d875620e82077790238686bc62695393bc70c
adenuga adewumi <adenugaadewumi01@gmail.com>
2024-08-16 13:27:02 +0100

    "Added toJSON transform to reviewSchema to rename _id to id, remove _id and __v, and add productCode to JSON output."

--- product.js
+++ product.js
@@ -30,6 +30,16 @@ productSchema.set("toJSON", {
   },
 });
 
+reviewSchema.set("toJSON", {
+  transform: (doc, ret) => {
+    ret.id = ret._id; // rename _id to id
+    delete ret._id; // remove _id
+    delete ret.__v; // remove __v
+    ret.productCode = doc.productCode; // add productCode to the JSON output
+    return ret;
+  },
+});
+
 const Product = mongoose.model("Product", productSchema);
 
 module.exports = Product;
