8df6d81b96d0c04ac84d89b7df15094a2c30f08b
adenuga adewumi <adenugaadewumi01@gmail.com>
2024-08-16 03:00:21 +0100

    Remove useNewUrlParser and useUnifiedTopology options from mongoose connection.

--- index.js
+++ index.js
@@ -13,10 +13,7 @@ app.use(bodyParser.json());
 app.use(morgan("dev"));
 
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => {
     console.error("MongoDB connection error:", err);
